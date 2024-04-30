f78fb0e4e8304efd8cddb1a116dabf4405c6aa70
enesorhan <enestcp@gmail.com>
2024-04-30 11:26:09 +0200

    working on form in create html

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.User;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @GetMapping("/create")
-    public String createUser(){
+    public String createUser(Model model){
+
+        model.addAttribute("user", new UserDTO());
+
+
         return "/user/create";
     }
 }
